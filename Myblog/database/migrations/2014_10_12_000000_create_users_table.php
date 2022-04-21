95f81f14687b7ccf1233fd9c6686139c682b64df
miracezra <miracergenc@gmail.com>
2022-04-21 18:24:37 +0300

    myblog oluşumu yapıld

--- Myblog/database/migrations/2014_10_12_000000_create_users_table.php
+++ Myblog/database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,9 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('category', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('category_name');
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -27,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('category');
+        Schema::dropIfExists('users');
     }
 };
